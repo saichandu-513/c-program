7d53d405a32550b0bca4318a3bba499c1d5c3332
saichandu-513 <saichandugovindalapudi@gmail.com>
2024-10-25 12:00:59 +0530

    Create number is equal to zero,positive,negative.c

---
+++ number is equal to zero,positive,negative.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter n value");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive");
+    }
+    else if(n==0)
+    {
+        printf("zero");
+        
+    }
+            else
+            {
+                printf("negative");
+                
+            }
+}
