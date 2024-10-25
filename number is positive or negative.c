bd668fbaace7f3d9994cf4f32fda52be46842c3a
saichandu-513 <saichandugovindalapudi@gmail.com>
2024-10-25 11:57:49 +0530

    Create number is positive or negative.c

---
+++ number is positive or negative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter n value");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive");
+    }
+            else
+            {
+                printf("negative");
+                
+            }
+}
